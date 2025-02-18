0dafc07b1e3573ba11e130362bd10e2862ee0722
alikaankoc1 <kocalikaan564@gmail.com>
2025-02-18 14:24:40 +0300

    JavaScript output methods

---
+++ 2-Çıktı Alma/app.js
@@ -0,0 +1,22 @@
+//!   JavaScript ile çıktı alma yöntemleri
+
+// document.write ile çıktı alma
+document.write("Ali Kaan Koç <br>");
+document.write("Dumlupınar Üniversitesi Bilgisayar Mühendisliği öğrencisiyim. <br>");
+document.write("Kariyer hedefim Full-Stack Develepor olmak.");
+
+
+
+// console.log ile çıktı alma
+console.log("Merhaba Dünya");
+let a = 5;
+let b = 17;
+console.log("İki sayının toplamı:" ,(a+b));
+console.log("İki sayının farkı:" ,(a-b));
+console.log(document.location.host);
+console.log(document.location.hostname);
+
+
+// alert ile çıktı alma
+alert("Web Siteme Hoşgeldiniz.")
+alert("Benimle iletişime geçebilirsiniz.")
